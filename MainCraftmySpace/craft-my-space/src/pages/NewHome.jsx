2d9c90bb92926e24a2d5a06e96f8b22ae11961ff
Vishwa2011 <vishwaitaliya23@gmail.com>
2025-04-28 18:02:35 +0530

    changes

--- MainCraftmySpace/craft-my-space/src/pages/NewHome.jsx
+++ MainCraftmySpace/craft-my-space/src/pages/NewHome.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { FiMenu, FiSearch } from "react-icons/fi";
 import Footers from "./Footer";
+import { AiOutlineClose } from 'react-icons/ai';
 
 const NewHome = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
